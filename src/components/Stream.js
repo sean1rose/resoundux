f939052a6a313c89a6df99000589257b199ed419
Sean Rose <sean1rose@gmail.com>
2016-11-06 21:56:10 -0800

    Refactor stream component to be stateless functional

--- src/components/Stream.js
+++ src/components/Stream.js
@@ -1,5 +1,23 @@
 import React from 'react';
 
+// stateless functional component (sole function is to take in an input and spit out an output)
+function Stream({ tracks = [] }) {
+	return (
+		<div>
+			{
+				tracks.map((track, key) => {
+					return <div className="track" key={key}>{track.title}</div>;
+				})
+			}
+		</div>
+	);
+}
+
+
+/* 
+
+	//	*** ES6 class component (if component had state or if you need component lifecycle methods) ***
+
 class Stream extends React.Component {
 
 	render() {
@@ -13,9 +31,10 @@ class Stream extends React.Component {
 					})
 				}
 			</div>
-		)
+		);
 	}
 
 }
+*/
 
 export default Stream;
